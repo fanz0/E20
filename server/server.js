96933c7b052082b389491f7cfe493fc53108960f
Claudio <99316714+fanz0@users.noreply.github.com>
2024-03-07 14:37:33 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -72,6 +72,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/events", eventRoute);
 app.use("/api/user", userRoute);
 
+app.get("/", (req, res) => {
+    res.send("Hello")
+})
+
 app.listen(3000);
 
 mongoose
